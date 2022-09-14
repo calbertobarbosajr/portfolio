415538c7add879ba3aac4d4a58db4bf83b84b014
Carlos Alberto Barbosa Junior <calbertobarbosajr@gmail.com>
2022-09-13 22:29:42 -0300

    testando

---
+++ index_htm_files/1.js
@@ -0,0 +1,29 @@
+window.local_slideshowLeft_3_data = [
+{
+ autoscroll: "5"
+},
+{
+ image: "index_htm_files/280.png",
+ thumb: "index_htm_files/281.png"
+},
+{
+ image: "index_htm_files/282.png",
+ thumb: "index_htm_files/283.png"
+},
+{
+ image: "index_htm_files/284.png",
+ thumb: "index_htm_files/285.png"
+},
+{
+ image: "index_htm_files/286.png",
+ thumb: "index_htm_files/287.png"
+},
+{
+ image: "index_htm_files/288.png",
+ thumb: "index_htm_files/289.png"
+},
+{
+ image: "index_htm_files/290.png",
+ thumb: "index_htm_files/291.png"
+}
+];
